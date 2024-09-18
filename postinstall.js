67ff2b35241da2c4e531886dee294f5d1cc4a2e3
Lucas Manuel <155545753+Lucasmanuelmz@users.noreply.github.com>
2024-09-18 19:56:16 +0200

    Corrigi a forma como importava a meta dirname em js no postinstaçç.js

--- postinstall.js
+++ postinstall.js
@@ -1,5 +1,6 @@
 import fse from 'fs-extra';
 import path from 'path';
-const topDir = import.meta.dirname;
+import { fileURLToPath } from 'url';
+const topDir = path.dirname(fileURLToPath(import.meta.url));
 fse.emptyDirSync(path.join(topDir, 'public', 'tinymce'));
 fse.copySync(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce'), { overwrite: true });
